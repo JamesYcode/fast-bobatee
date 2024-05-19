5837eab1321e6e4c0c5283e7b1b0aa39cb1dae4b
James <JamesYcode@gmail.com>
2024-05-18 21:59:35 -0400

    search order component.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 
 function Username() {
   const username = useSelector((store) => store.user.username);
+  // * Allows search bar component to be either in the middle if there is a username or right side if there is no username
   if (!username) return null;
   return (
     <div className='hidden text-sm font-semibold md:block'>{username}</div>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import Username from '../features/user/Username';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
     <header className='flex items-center justify-between border-b border-stone-200 bg-yellow-200 px-4 py-3 uppercase sm:px-6'>
       <Link to='/'>Fast BobaTea</Link>
-      {/* <SearchOrder /> */}
+      <SearchOrder />
       <Username />
     </header>
   );
