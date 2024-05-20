b599faeb96cc9c4ecfc7c962c5f880ce7d78435a
James <JamesYcode@gmail.com>
2024-05-20 18:22:44 -0400

    disable auto zoon in input tag - safari on iphones.

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -5,7 +5,7 @@ function Username() {
   // * Allows search bar component to be either in the middle if there is a username or right side if there is no username
   if (!username) return null;
   return (
-    <div className='hidden text-base font-semibold md:block'>{username}</div>
+    <div className='hidden text-sm font-semibold md:block'>{username}</div>
   );
 }
 
